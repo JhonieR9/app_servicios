a769146b37a7912f586b3405343eb40b99e6cebf
JhonieR9 <jhoniercespedes32@gmail.com>
2026-05-02 17:43:02 -0500

    Limpiar scripts temporales

--- find_btn.py
+++
@@ -1,9 +0,0 @@
-with open('templates/trabajadores/registro_trabajador.html', 'rb') as f:
-    raw = f.read()
-c = raw.decode('utf-8')
-
-# Buscar el botón de guardar
-idx = c.find('Guardar Registro')
-print(f'Guardar Registro en: {idx}')
-if idx != -1:
-    print(repr(c[idx-200:idx+100]))
